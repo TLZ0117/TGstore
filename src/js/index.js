4ac650dc5c2169d23cda2f1404d843039a1c924d
thomas@global <thomas@global.com>
2017-12-08 10:34:12 +0800

    跟踪src文件夹

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+
+window.onload = function(){
+    var oTophead = document.getElementsByClassName('tophead');
+    new Carousel({
+        el: document.getElementsByClassName('carousel')[0]
+    });
+
+}         
